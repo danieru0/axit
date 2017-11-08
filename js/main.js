ac51d1bc4f3253cfe20618f59a848897a991f8b4
Daniel Dąbrowski <elosik123@onet.pl>
2017-11-08 16:04:05 +0100

    added features section with working tab buttons

--- js/main.js
+++ js/main.js
@@ -1,10 +1,23 @@
-const btn = document.getElementById('nav-hamburger'),
-      menu = document.getElementById('menu');
+(function() {
+    const btn = document.getElementById('nav-hamburger'),
+          menu = document.getElementById('menu'),
+          featuresBtn = document.getElementsByClassName('features-btn'),
+          tabs = document.getElementsByClassName('features-tab');
 
-btn.addEventListener('click', function() {
-    if (menu.classList.contains('active')) {
-        menu.classList.remove('active');
-    } else {
-        menu.classList.add('active');
+    btn.addEventListener('click', function() {
+        if (menu.classList.contains('active')) {
+            menu.classList.remove('active');
+        } else {
+            menu.classList.add('active');
+        }
+    });
+
+    for (var i = 0; i < featuresBtn.length; i++) {
+        featuresBtn[i].addEventListener('click', function() {
+            document.querySelectorAll('.features-tab.active').forEach(function(item) {
+                item.classList.remove('active');
+            });
+            tabs[this.value].className += ' active';
+        });
     }
-});
+})();
